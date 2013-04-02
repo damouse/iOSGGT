2dccc8c3e60a9cf1db0a65262b3e65a393da5df8
damouse <barboi@wisc.edu>
2013-04-01 19:39:41 -0500

    Removed the readme from appdelegate into its own file

--- iOSGGT/AppDelegate.m
+++ iOSGGT/AppDelegate.m
@@ -6,18 +6,6 @@
 //  Copyright (c) 2013 Mihnea Barboi. All rights reserved.
 //
 
-/**
- *  NOTES:
- *  -add an actual day counter to the root section so its a little bit easier to see how much you have to spend in x days
- *  -Holy crap, Gdocs has an extensive API for accessing spreadsheets. Consider importing to that, or possibly using that?
- *
- *  BUGS:
- *
- *
- *  TODO:
- *  -use CHCSVParser to parse the CSV, consider writing something serverside to convert the spreadsheets
- *
- */
 #import "AppDelegate.h"
 
 @implementation AppDelegate
